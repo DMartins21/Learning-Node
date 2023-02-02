208d8c7df551b6250086cbbbbe7eb53d1fade19d
Davi <davi_martins2009@outlook.com>
2023-02-02 16:02:42 -0300

    Manipulando Formularios

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import Nome from "./Components/nome";
 
 function App() {
 
-  const [aluno, setAluno] = useState('Mateus')
+  const [nome,setNome] = useState('')
+  const [email,setEmail] = useState('')
+  const [idade,setIdade] = useState('')
 
-  function handleChageName(nome){
-    setAluno(nome)
+  const [user,setUser] = useState()
+
+  function handleRegistro(event){
+    event.preventDefault()
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+  })
   }
 
   return (
    <div>
-    <h1>Componente App</h1>
+    <h1>Cadastrando Usuario</h1>
+    <form onSubmit={handleRegistro}>
+      <label>Nome:</label><br/>
+      <input placeholder="Digite seu Nome" 
+      value={nome} onChange={(event) => setNome(event.target.value)}
+      required/><br/>
+      
+      <label>Email:</label><br/>
+      <input placeholder="Digite seu Email" 
+      value={email} onChange={(event) => setEmail(event.target.value)}
+      required/><br/>
+
+      <label>Idade:</label><br/>
+      <input placeholder="Digite seu Idade"
+      value={idade} onChange={(event) => setIdade(event.target.value)}
+      required/><br/>
+
+      <button type="submit">Registrar</button>
+    </form>
     <br/>
-    <h2>Olá:{aluno}</h2>
-    <button onClick={() => handleChageName('Lucas Oliveira')}>
-      Mudar nome
-    </button>
-    <Nome aluno="Lucas" idade={30}/>
-    <br/>
-    <Nome aluno="Matheus" idade={25}/>
+    <div>
+      <span>Bem vindo: {user.nome}</span><br/>
+      <span>Idade: {user.idade}</span><br/>
+      <span>Email: {user.email}</span><br/>
+    </div>
    </div>
   );
 }
