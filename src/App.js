d62933bf2a5ca2a6b4b9f7704ba7d329b997e238
Davi <davi_martins2009@outlook.com>
2023-02-01 23:06:02 -0300

    Trabalhando com UseState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Nome from "./Components/nome";
 
 function App() {
+
+  const [aluno, setAluno] = useState('Mateus')
+
+  function handleChageName(nome){
+    setAluno(nome)
+  }
+
   return (
    <div>
     <h1>Componente App</h1>
+    <br/>
+    <h2>Olá:{aluno}</h2>
+    <button onClick={() => handleChageName('Lucas Oliveira')}>
+      Mudar nome
+    </button>
     <Nome aluno="Lucas" idade={30}/>
     <br/>
     <Nome aluno="Matheus" idade={25}/>
