c209a121cf8b84b483d395aead8e1e9bdc9105e3
Davi <davi_martins2009@outlook.com>
2023-02-02 16:13:12 -0300

    Praticando

--- src/App.js
+++ src/App.js
@@ -3,47 +3,35 @@ import Nome from "./Components/nome";
 
 function App() {
 
-  const [nome,setNome] = useState('')
-  const [email,setEmail] = useState('')
-  const [idade,setIdade] = useState('')
+  const[input, setInput] = useState('')
+  const[tarefas, setTarefas] = useState([])
 
   const [user,setUser] = useState()
 
   function handleRegistro(event){
     event.preventDefault()
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-  })
+    setTarefas([...tarefas,input])
+    setInput('')
   }
 
   return (
    <div>
     <h1>Cadastrando Usuario</h1>
     <form onSubmit={handleRegistro}>
-      <label>Nome:</label><br/>
+      <label>Nome da Tarefa:</label><br/>
       <input placeholder="Digite seu Nome" 
-      value={nome} onChange={(event) => setNome(event.target.value)}
-      required/><br/>
-      
-      <label>Email:</label><br/>
-      <input placeholder="Digite seu Email" 
-      value={email} onChange={(event) => setEmail(event.target.value)}
-      required/><br/>
-
-      <label>Idade:</label><br/>
-      <input placeholder="Digite seu Idade"
-      value={idade} onChange={(event) => setIdade(event.target.value)}
+      value={input} onChange={(event) => setInput(event.target.value)}
       required/><br/>
 
       <button type="submit">Registrar</button>
     </form>
     <br/>
     <div>
-      <span>Bem vindo: {user.nome}</span><br/>
-      <span>Idade: {user.idade}</span><br/>
-      <span>Email: {user.email}</span><br/>
+      {tarefas.map(tarefa => (
+        <ul>
+          <li key={tarefa}>{tarefa}</li>
+        </ul>
+      ))}
     </div>
    </div>
   );
