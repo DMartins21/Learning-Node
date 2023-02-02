fe6d63efe92897a1fa598d396dae2e3152e33702
Davi <davi_martins2009@outlook.com>
2023-02-01 22:41:25 -0300

    Entendendo Props e componentes - React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Nome from "./Components/nome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>Componente App</h1>
+    <Nome aluno="Lucas" idade={30}/>
+    <br/>
+    <Nome aluno="Matheus" idade={25}/>
+   </div>
   );
 }
 
